0ccc05792f6f6c204eaa11b4de56447ea988b83d
Phongle1311 <hoaiphong13.11.2002@gmail.com>
2023-06-29 14:16:06 +0700

    lightmode

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { Box, Typography, TextField, Paper } from '@mui/material'
+import { Box, Typography, TextField, Paper, Button, createTheme, ThemeProvider } from '@mui/material'
 import axios from 'axios'
-import useAppSnackbar from '../../lib/hooks/useAppSnackBar'
 import LoadingButton from '../../lib/components/LoadingButton/LoadingButton'
 
 const HomePage = () => {
@@ -9,7 +8,7 @@ const HomePage = () => {
   const [context, setContext] = useState('')
   const [question, setQuestion] = useState('')
   const [chatHistory, setChatHistory] = useState([])
-  const { showSnackbarError } = useAppSnackbar()
+  const [darkMode, setDarkMode] = useState(false)
   const chatContainerRef = useRef(null)
 
   const handleContextChange = (event) => {
@@ -47,7 +46,7 @@ const HomePage = () => {
       setChatHistory((prevChatHistory) => [...prevChatHistory, newChat, newAnswer])
       setQuestion('')
     } catch (error) {
-      showSnackbarError(error)
+      console.log('Error:', error)
     } finally {
       setWaiting(false)
     }
@@ -81,105 +80,103 @@ const HomePage = () => {
     }
   }, [chatHistory])
 
-  return (
-    <Box sx={{ display: 'flex', height: '100vh' }}>
-      {/* Left Sidebar */}
-      <Box sx={{ flexBasis: '300px', backgroundColor: '#f5f5f5', padding: '1rem' }}>
-        <Typography variant='h6' sx={{ marginBottom: '1rem' }}>
-          Chat History
-        </Typography>
-        <Box sx={{ maxHeight: 'calc(100vh - 160px)', overflowY: 'auto' }}>
-          {chatHistory.map((chat, index) => (
-            <Box
-              key={index}
-              sx={{
-                marginBottom: '0.5rem',
-                backgroundColor: chat.role === 'user' ? '#e1f5fe' : '#f3e5f5',
-                padding: '0.5rem',
-                borderRadius: '8px'
-              }}
-            >
-              <Typography variant='subtitle1' sx={{ fontWeight: 'bold' }}>
-                {chat.role === 'user' ? 'User:' : 'Bot:'}
-              </Typography>
-              <Typography>{chat.content}</Typography>
-            </Box>
-          ))}
-        </Box>
-      </Box>
+  const toggleDarkMode = () => {
+    setDarkMode((prevDarkMode) => !prevDarkMode)
+  }
 
-      {/* Main Content */}
-      <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+      background: {
+        default: darkMode ? '#000000' : '#ffffff'
+      },
+      text: {
+        primary: darkMode ? '#ffffff' : '#000000'
+      }
+    }
+  })
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
         {/* Header */}
         <Box
           sx={{
-            backgroundColor: '#f5f5f5',
-            padding: '1rem',
-            borderBottom: '1px solid #e0e0e0',
-            flexShrink: 0
+            backgroundColor: darkMode ? '#212121' : '#f5f5f5',
+            color: darkMode ? '#ffffff' : '#000000',
+            padding: '1rem'
           }}
         >
           <Typography variant='h4'>WebApp Demo</Typography>
+          <Button variant='contained' onClick={toggleDarkMode}>
+            {darkMode ? 'Light Mode' : 'Dark Mode'}
+          </Button>
         </Box>
 
-        {/* Chat Container */}
-        <Box sx={{ flex: 1, padding: '1rem', overflowY: 'auto' }} ref={chatContainerRef}>
-          {chatHistory.map((chat, index) => (
-            <Box
-              key={index}
-              sx={{
-                display: 'flex',
-                flexDirection: chat.role === 'user' ? 'row-reverse' : 'row',
-                marginBottom: '1rem',
-                alignItems: 'flex-start'
-              }}
-            >
-              <Box sx={{ marginRight: chat.role === 'user' ? '0' : '1rem' }}>
-                <Paper
-                  elevation={2}
-                  sx={{
-                    backgroundColor: chat.role === 'user' ? '#e1f5fe' : '#f3e5f5',
-                    padding: '0.5rem',
-                    borderRadius: '8px'
-                  }}
-                >
-                  <Typography>{chat.content}</Typography>
-                </Paper>
+        {/* Main Content */}
+        <Box
+          sx={{ flex: 1, display: 'flex', flexDirection: 'column', backgroundColor: darkMode ? '#333333' : '#f5f5f5' }}
+        >
+          {/* Chat Container */}
+          <Box sx={{ flex: 1, padding: '1rem', overflowY: 'auto' }} ref={chatContainerRef}>
+            {chatHistory.map((chat, index) => (
+              <Box
+                key={index}
+                sx={{
+                  display: 'flex',
+                  flexDirection: chat.role === 'user' ? 'row-reverse' : 'row',
+                  marginBottom: '1rem',
+                  alignItems: 'flex-start'
+                }}
+              >
+                <Box sx={{ marginRight: chat.role === 'user' ? '0' : '1rem' }}>
+                  <Paper
+                    elevation={2}
+                    sx={{
+                      backgroundColor: chat.role === 'user' ? '#e1f5fe' : '#f3e5f5',
+                      padding: '0.5rem',
+                      borderRadius: '8px',
+                      color: darkMode ? '#000000' : '#000000'
+                    }}
+                  >
+                    <Typography>{chat.content}</Typography>
+                  </Paper>
+                </Box>
               </Box>
-            </Box>
-          ))}
-        </Box>
-
-        {/* Footer */}
-        <Box sx={{ padding: '1rem', borderTop: '1px solid #e0e0e0', flexShrink: 0 }}>
-          <Box sx={{ display: 'flex' }}>
-            <input type='file' onChange={handleFileUpload} accept='.txt' />
-            <TextField
-              fullWidth
-              multiline
-              value={context}
-              onChange={handleContextChange}
-              placeholder='Enter context ...'
-              variant='outlined'
-              sx={{ marginRight: '1rem' }}
-            />
+            ))}
           </Box>
 
-          <Box sx={{ display: 'flex', marginTop: '1rem' }}>
-            <TextField
-              fullWidth
-              value={question}
-              onChange={handleQuestionChange}
-              placeholder='Enter question ...'
-              variant='outlined'
-            />
-            <LoadingButton loading={waiting} color='primary' variant='contained' onClick={handleAnswering}>
-              Gửi
-            </LoadingButton>
+          {/* Footer */}
+          <Box sx={{ padding: '1rem', borderTop: '1px solid #e0e0e0', flexShrink: 0 }}>
+            <Box sx={{ display: 'flex' }}>
+              <input type='file' onChange={handleFileUpload} accept='.txt' />
+              <TextField
+                fullWidth
+                multiline
+                value={context}
+                onChange={handleContextChange}
+                placeholder='Enter context ...'
+                variant='outlined'
+                sx={{ marginRight: '1rem' }}
+              />
+            </Box>
+
+            <Box sx={{ display: 'flex', marginTop: '1rem' }}>
+              <TextField
+                fullWidth
+                value={question}
+                onChange={handleQuestionChange}
+                placeholder='Enter question ...'
+                variant='outlined'
+              />
+              <LoadingButton loading={waiting} color='primary' variant='contained' onClick={handleAnswering}>
+                Gửi
+              </LoadingButton>
+            </Box>
           </Box>
         </Box>
       </Box>
-    </Box>
+    </ThemeProvider>
   )
 }
 
