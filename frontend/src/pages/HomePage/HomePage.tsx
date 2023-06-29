10c6e471dafe0e91abdcbc56aca745212a8d5c6d
Phongle1311 <hoaiphong13.11.2002@gmail.com>
2023-06-29 14:04:38 +0700

    input file

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,15 +1,16 @@
-import { Box, TextField, Typography } from '@mui/material'
+import React, { useState, useRef, useEffect } from 'react'
+import { Box, Typography, TextField, Paper } from '@mui/material'
 import axios from 'axios'
-import { useState } from 'react'
-import LoadingButton from '../../lib/components/LoadingButton/LoadingButton'
 import useAppSnackbar from '../../lib/hooks/useAppSnackBar'
+import LoadingButton from '../../lib/components/LoadingButton/LoadingButton'
 
 const HomePage = () => {
   const [waiting, setWaiting] = useState(false)
   const [context, setContext] = useState('')
   const [question, setQuestion] = useState('')
-  const [answer, setAnswer] = useState('')
+  const [chatHistory, setChatHistory] = useState([])
   const { showSnackbarError } = useAppSnackbar()
+  const chatContainerRef = useRef(null)
 
   const handleContextChange = (event) => {
     setContext(event.target.value)
@@ -34,8 +35,17 @@ const HomePage = () => {
           }
         }
       )
-      setAnswer(response.data.answer)
-      // setAnswer(response.data.summary_text)
+      const answer = response.data.answer
+      const newChat = {
+        role: 'user',
+        content: question
+      }
+      const newAnswer = {
+        role: 'bot',
+        content: answer
+      }
+      setChatHistory((prevChatHistory) => [...prevChatHistory, newChat, newAnswer])
+      setQuestion('')
     } catch (error) {
       showSnackbarError(error)
     } finally {
@@ -43,33 +53,107 @@ const HomePage = () => {
     }
   }
 
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0]
+
+    // Check if file size exceeds the limit (e.g., 5MB)
+    const fileSizeLimit = 5 * 1024 * 1024 // 5MB in bytes
+    if (file.size > fileSizeLimit) {
+      // Show an error message or notification to the user
+      console.log('File size exceeds the limit.')
+      return
+    }
+
+    const reader = new FileReader()
+    reader.readAsText(file)
+    reader.onload = () => {
+      setContext(reader.result.toString())
+    }
+    reader.onerror = () => {
+      console.log('Error on reading file: ', reader.error)
+    }
+  }
+
+  useEffect(() => {
+    // Auto scroll to bottom of chat container
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight
+    }
+  }, [chatHistory])
+
   return (
-    <Box sx={{ padding: '1rem' }}>
-      <header>
+    <Box sx={{ display: 'flex', height: '100vh' }}>
+      {/* Left Sidebar */}
+      <Box sx={{ flexBasis: '300px', backgroundColor: '#f5f5f5', padding: '1rem' }}>
+        <Typography variant='h6' sx={{ marginBottom: '1rem' }}>
+          Chat History
+        </Typography>
+        <Box sx={{ maxHeight: 'calc(100vh - 160px)', overflowY: 'auto' }}>
+          {chatHistory.map((chat, index) => (
+            <Box
+              key={index}
+              sx={{
+                marginBottom: '0.5rem',
+                backgroundColor: chat.role === 'user' ? '#e1f5fe' : '#f3e5f5',
+                padding: '0.5rem',
+                borderRadius: '8px'
+              }}
+            >
+              <Typography variant='subtitle1' sx={{ fontWeight: 'bold' }}>
+                {chat.role === 'user' ? 'User:' : 'Bot:'}
+              </Typography>
+              <Typography>{chat.content}</Typography>
+            </Box>
+          ))}
+        </Box>
+      </Box>
+
+      {/* Main Content */}
+      <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+        {/* Header */}
         <Box
           sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            marginBottom: '1rem'
+            backgroundColor: '#f5f5f5',
+            padding: '1rem',
+            borderBottom: '1px solid #e0e0e0',
+            flexShrink: 0
           }}
         >
           <Typography variant='h4'>WebApp Demo</Typography>
-          {/* Add navigation buttons or components here */}
         </Box>
-      </header>
 
-      <main>
-        <section>
-          <Typography variant='h6'>Question Answering</Typography>
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              marginBottom: '1rem'
-            }}
-          >
+        {/* Chat Container */}
+        <Box sx={{ flex: 1, padding: '1rem', overflowY: 'auto' }} ref={chatContainerRef}>
+          {chatHistory.map((chat, index) => (
+            <Box
+              key={index}
+              sx={{
+                display: 'flex',
+                flexDirection: chat.role === 'user' ? 'row-reverse' : 'row',
+                marginBottom: '1rem',
+                alignItems: 'flex-start'
+              }}
+            >
+              <Box sx={{ marginRight: chat.role === 'user' ? '0' : '1rem' }}>
+                <Paper
+                  elevation={2}
+                  sx={{
+                    backgroundColor: chat.role === 'user' ? '#e1f5fe' : '#f3e5f5',
+                    padding: '0.5rem',
+                    borderRadius: '8px'
+                  }}
+                >
+                  <Typography>{chat.content}</Typography>
+                </Paper>
+              </Box>
+            </Box>
+          ))}
+        </Box>
+
+        {/* Footer */}
+        <Box sx={{ padding: '1rem', borderTop: '1px solid #e0e0e0', flexShrink: 0 }}>
+          <Box sx={{ display: 'flex' }}>
+            <input type='file' onChange={handleFileUpload} accept='.txt' />
             <TextField
               fullWidth
               multiline
@@ -77,45 +161,24 @@ const HomePage = () => {
               onChange={handleContextChange}
               placeholder='Enter context ...'
               variant='outlined'
-              sx={{
-                marginRight: '1rem'
-              }}
+              sx={{ marginRight: '1rem' }}
             />
+          </Box>
 
+          <Box sx={{ display: 'flex', marginTop: '1rem' }}>
             <TextField
               fullWidth
               value={question}
               onChange={handleQuestionChange}
               placeholder='Enter question ...'
               variant='outlined'
-              sx={{
-                marginRight: '1rem'
-              }}
             />
-
             <LoadingButton loading={waiting} color='primary' variant='contained' onClick={handleAnswering}>
               Gửi
             </LoadingButton>
           </Box>
-
-          <Box>
-            <Typography variant='h6'>Answer</Typography>
-            <Typography>{answer}</Typography>
-          </Box>
-        </section>
-
-        <section>
-          <Typography variant='h6'>Help</Typography>
-          {/* Add help content here */}
-        </section>
-
-        <section>
-          <Typography variant='h6'>Introduction</Typography>
-          {/* Add introduction content here */}
-        </section>
-      </main>
-
-      <footer>{/* Add footer content here */}</footer>
+        </Box>
+      </Box>
     </Box>
   )
 }
